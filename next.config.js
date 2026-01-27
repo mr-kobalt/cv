e262d4f83b777641a449ae94bfa195766bd16fdc
Vasiliy Kovalchuk <v@kov.app>
2026-01-27 14:18:54 +0300

    Fix redirections for object storage hosting (trailingSlash)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   images: {
     unoptimized: true, // Disable optimization for static export
   },
