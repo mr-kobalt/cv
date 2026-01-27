806aedee49601ab76a16829376778a615951bbe1
Vasiliy Kovalchuk <v@kov.app>
2026-01-27 10:32:35 +0300

    Fix image loading with `output: "export"` nextjs option (enable unoptimized images)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // Disable optimization for static export
+  },
 };
 
 module.exports = nextConfig
